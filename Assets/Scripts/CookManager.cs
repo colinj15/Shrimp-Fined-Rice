a6ecffafaae6842e3e60fecff353f90f69680e26
Colin Johnson <cjohns15@trinity.edu>
2025-10-27 20:01:06 -0500

    Basic cooking minigame functionality

---
+++ Assets/Scripts/CookManager.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+
+public class CookManager : MonoBehaviour
+{
+    public float seconds;
+    public TextMeshProUGUI time;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (seconds > 0)
+        {
+            // Decrease once per second
+            seconds -= Time.deltaTime;
+
+            // Ensure time doesn’t go negative
+            if (seconds < 0)
+                seconds = 0;
+        }
+
+        // Update time display
+        time.text = Mathf.CeilToInt(seconds).ToString();
+    }
+
+    public bool IsTimeUp()
+    {
+        return seconds <= 0;
+    }
+
+    public int GetTime()
+    {
+        return Mathf.CeilToInt(seconds);
+    }
+}
---
+++ Assets/Scripts/MeterController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MeterController : MonoBehaviour
+{
+    public SpriteRenderer heat;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
