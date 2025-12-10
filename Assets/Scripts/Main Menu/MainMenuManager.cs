9f06c689b4c4b69dfd3495e49ed3da1bd3593f27
Colin Johnson <cjohns15@trinity.edu>
2025-12-10 17:05:11 -0600

    Main menu functional and updated background for lobby and waiting area

---
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Canvas")]
+    public Canvas main;
+    public Canvas instructions;
+    public Canvas credits;
+    private List<Canvas> canvas; 
+
+    [Header("Instructions")]
+    public TextMeshProUGUI instructionText;
+    public List<string> instructionList;
+    private int activeInstruction = 0;
+
+    void Awake()
+    {
+        canvas = new List<Canvas>();
+        canvas.Add(main);
+        canvas.Add(instructions);
+        canvas.Add(credits);    
+        SetCanvas(main);
+        UpdateInstructions();
+    }
+
+   public void Play()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+        
+    public void Instructions()
+    {
+        SetCanvas(instructions);
+    }
+
+    public void Credits()
+    {
+        SetCanvas(credits);
+    }
+    
+    public void Back()
+    {
+        SetCanvas(main);
+    }
+
+    public void UpdateInstructions()
+    {
+        instructionText.text = instructionList[activeInstruction];
+    }
+
+    public void Next()
+    {
+        if (activeInstruction < instructionList.Count - 1) activeInstruction++;
+        UpdateInstructions();
+    }
+
+    public void Prev()
+    {
+        if (activeInstruction > 0) activeInstruction--;
+        UpdateInstructions();
+    }
+
+    public void SetCanvas(Canvas c)
+    {
+        for (int i = 0; i < canvas.Count; i++)
+        {
+            if (c == canvas[i]) canvas[i].enabled = true;
+            else canvas[i].enabled = false;
+        }
+    }
+}
